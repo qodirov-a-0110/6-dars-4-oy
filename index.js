ab0379f7c7adcac681186d67538ee243b384d6ab
qodirov-a-0110 <qodirovabdumajid03@gmail.com>
2024-07-27 15:57:22 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,86 @@
+// masala
+// let arr = [1, 2, 3, 4];
+// let newArr = [];
+// for (let i = 0; i <= arr; i++) {
+//   newArr.push(arr[i]);
+// }
+// console.log(newArr);
+
+// masala
+// let arr1 = [1,2,3];
+// let arr2 = [4,5,6];
+// let arr = arr1.concat(arr2)
+// let sum = arr[0];
+// for(let i = 1; i < arr.length; i++) {
+//     sum += arr[i]
+// }
+// console.log(sum);
+
+//masala
+// let arr = [1,2,3,4];
+// let newArr = [];
+// for (const iterator of arr) {
+//     newArr.unshift(iterator)
+// }
+// console.log(newArr);
+
+// masala
+// let arr = [5,6,7,8,9];
+// let min = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         min = arr[i]
+//     }
+// }
+// console.log(min ** 4);
+
+// masala
+// let arr = [1,2,3,4,5];
+// arr.sort(function(a,b) {
+//     return b - a
+// })
+// console.log(arr[1]);
+
+// masala
+
+// let arr = [1,2,3,'salom'];
+// let res = [];
+// for (let i = 0; i < arr.length; i++) {
+//     if (typeof arr[i] == 'string') {
+//         res = arr[i].length
+//     }
+// }
+// console.log(res);
+
+// masala
+// let arr = [1,2,3,4];
+// let newArr = [];
+// for (const iterator of arr) {
+//     if (iterator % 2 == 1) {
+//         newArr.unshift(iterator)
+//     }
+// }
+// console.log(newArr);
+
+// masala
+// let arr = [10,25,30,45];
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 3 == 0) {
+//         newArr.push(arr[i])
+//     }
+// }
+// console.log(newArr);
+
+// masala
+// let arr = [1,2,3,4,5];
+// let newArr = [];
+// let sum = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//     if ()
+// }
+
+//masala
+// const myArray = [1, 2, 3, 4, 5];
+// const result = swapElements(myArray);
+// console.log(result);
